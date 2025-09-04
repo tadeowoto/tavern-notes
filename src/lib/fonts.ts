4463b6bad518ba9e15951b33ef4755fd578f0d2e
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-09-04 17:27:23 -0300

    feat: agregado las fuentes, colores y archivos estaticos

---
+++ src/lib/fonts.ts
@@ -0,0 +1,6 @@
+import { Cinzel_Decorative, Merriweather, Inter, JetBrains_Mono } from 'next/font/google'
+
+export const cinzel = Cinzel_Decorative({ subsets: ['latin'], weight: ['400','700'] })
+export const merriweather = Merriweather({ subsets: ['latin'], weight: ['400','700'] })
+export const inter = Inter({ subsets: ['latin'], weight: ['400','600','700'] })
+export const jetbrains = JetBrains_Mono({ subsets: ['latin'], weight: ['400','700'] })
